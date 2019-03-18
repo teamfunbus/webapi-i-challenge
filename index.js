6cbbd4ee992f3d5469146d0cb32c91f7c26ffc4e
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-03-18 13:47:23 -0500

    :construction: Scaffold the initial express server

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // implement your API here
+const db = require('./data/db');
+const express = require('express');
+const app = express();
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World!');
+})
+
+app.listen(4000, () => {
+  console.log(`The server is now running on port 4000`);
+})
