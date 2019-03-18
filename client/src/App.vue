1dfa85259b4ec26670789deb09595b2c9575e4c1
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-03-18 17:28:47 -0500

    :construction: Update the Home and App views to show current work

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,38 +1,27 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
+    <AppBar />
     <v-content>
-      <HelloWorld/>
+      <router-view/>
+      <AppFAB />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import AppBar from '@/components/AppBar.vue';
+import AppFAB from '@/components/AppFAB.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppBar,
+    AppFAB,
   },
-  data () {
+  data() {
     return {
       //
-    }
-  }
-}
+    };
+  },
+};
 </script>
