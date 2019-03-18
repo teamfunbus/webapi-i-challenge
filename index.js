1dbed969d90d92ca47cf864ed574a13e4f1a32f2
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-03-18 15:41:36 -0500

    :sparkles: Handle POST requests to create users @ "/api/users"

--- index.js
+++ index.js
@@ -14,6 +14,22 @@ app.get("/api/users", (req, res) => {
         .json({ error: "The users information could not be retrieved" });
     });
 });
+app.post("/api/users", (req, res) => {
+  const user = req.body;
+  if (user.name && user.bio) {
+    db.insert(user)
+      .then(user => res.status(201).json(user))
+      .catch(err => {
+        return res.status(500).json({
+          error: "There was an error while saving the user to the database"
+        });
+      });
+  } else {
+    return res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  }
+});
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello World!');
